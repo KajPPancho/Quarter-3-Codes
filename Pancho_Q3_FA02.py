708d87270a6ce93d96465192e6e358c5fba50b94
KajPPancho <kajarvin.pancho@smc.pshs.edu.ph>
2026-01-13 19:47:12 +0800

    Remove score summary print statements
    Removed print statements for maximum, average, and minimum scores of students.

--- Pancho_Q3_FA02.py
+++ Pancho_Q3_FA02.py
@@ -13,24 +13,3 @@ print("Scoresheet of 7 students in 5 subjects:")
 temp = enumerate(scores, start=1)
 for i, student in temp:
     print(f"Student {i}: Math  2 ={student[0]} Math 3 ={student[1]} Chemistry ={student[2]} Biology ={student[3]} Adtech ={student[4]} ")
-
-print()
-print("Maximum score of each student:")
-temp2 = enumerate(scores, start=1)
-for x, student in temp2:
-    l = x
-    max_score = max(student)
-    print(f"Student {l} : " ,max_score)
-print()
-print("Average Scores of each stuent:")
-for i, student in enumerate(scores, start=1):
-    avg = sum(student) / len(student)
-    print(f"Student {i} Average:" ,avg)
-
-print()
-print("Minimum score of each student:")
-temp3 = enumerate(scores, start=1)
-for a, student in temp3:
-    b = a
-    min_score = min(student)
-    print(f"Student {a} : " ,min_score)

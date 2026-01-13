fc5ffdcf464445b3feed409625655a226fe15048
KajPPancho <kajarvin.pancho@smc.pshs.edu.ph>
2026-01-13 21:48:43 +0800

    Update Pancho_Q3_FA02.py

--- Pancho_Q3_FA02.py
+++ Pancho_Q3_FA02.py
@@ -8,6 +8,10 @@ scores = [
     [100, 100, 99, 95, 96]
 ]
 
+print(scores[0])
+print(scores[4][3])
+print(scores[2][1])
+
 "I have learned that multi-dimensional array is very useful. 
 "It helps me in my JSON file and other data sheets. 
 "Although it is hard, the multidimeansional array is very useful to file up datas."

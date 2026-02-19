a63b9fbbd73bb2479f674b3df9134f7e1931aa04
KajPPancho <kajarvin.pancho@smc.pshs.edu.ph>
2026-02-19 20:16:29 +0800

    Refactor student dictionary and input handling

---
+++ Pancho_Q3_FA06.py
@@ -0,0 +1,15 @@
+
+stud = {}
+
+stud["name"] = input("Enter your name: ")
+stud["age"] = int(input("Enter your age: "))
+stud["sub"] = input("Enter your favorite subject: ")
+
+# Display the student's information
+print("Student Record:")
+print()
+print("Name:", stud["name"])
+print()
+print("Age:", stud["age"])
+print()
+print("Favorite Subject:", stud["sub"])

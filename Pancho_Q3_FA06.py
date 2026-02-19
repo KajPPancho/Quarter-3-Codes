2681368f3e62352d3e8771355bae0b9713123e21
KajPPancho <kajarvin.pancho@smc.pshs.edu.ph>
2026-02-19 20:18:00 +0800

    Updated
    Removed commented-out code for clarity.

--- Pancho_Q3_FA06.py
+++ Pancho_Q3_FA06.py
@@ -5,7 +5,7 @@ stud["name"] = input("Enter your name: ")
 stud["age"] = int(input("Enter your age: "))
 stud["sub"] = input("Enter your favorite subject: ")
 
-# Display the student's information
+
 print("Student Record:")
 print()
 print("Name:", stud["name"])
